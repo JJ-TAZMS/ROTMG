7e0c4657cb9e1ebe932e8df0092a707c1a031a73
James <jamesmichaelm1043@gmail.com>
2018-03-14 20:55:10 -0400

    LootBag
    Basic outline for the loot bag class. Need the item class. Need to figure out how to draw the bag.

--- ROTMG/src/game/LootBag.java
+++ ROTMG/src/game/LootBag.java
@@ -1,5 +1,32 @@
 package game;
-
+import java.util.ArrayList;
+import java.util.Random;
 public class LootBag {
-	//test
+	
+	ArrayList<String> bagItems; //array list of randomized item id's
+	
+	//create new lootbag item
+	//to be used in the enemy class when an enemy dies
+	public LootBag(String enemyTier) {
+		//create a new image
+		//SpriteSheet bag = new SpriteSheet();
+		
+		bagItems = new ArrayList<String>(); 
+		
+	}
+	
+	//drop the lootBag at the location that the enemy dies. Should be used in the enemy class?
+	public void dropBag(int xPos, int yPos) {
+		
+	}
+	
+	public void randomize() {
+		Random rand = new Random();
+
+		int numOfItems = rand.nextInt(4) + 1; //loot bag can spawn anywhere from 1 to 4 items
+		
+		for (int i=0; i<numOfItems; i++) {
+			//bagItems.add(random item from item class)
+		}
+	}
 }
