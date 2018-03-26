aca53788fb0f27351dfa5c28f0d6700fc686d272
James <jamesmichaelm1043@gmail.com>
2018-03-26 18:47:35 -0400

    Add files via upload
    change to loot bag class

--- ROTMG/src/game/LootBag.java
+++ ROTMG/src/game/LootBag.java
@@ -7,7 +7,7 @@ public class LootBag extends BufferedImageLoader{
 	
 	ArrayList<String> bagItems; //array list of randomized item id's
 	String tier;
-	
+	Item_Staves Item = new Item_Staves();
 	//create new lootbag item
 	//to be used in the enemy class when an enemy dies
 	public LootBag(String enemyTier, double xPos, double yPos) {
@@ -31,15 +31,12 @@ public class LootBag extends BufferedImageLoader{
 		Random rand = new Random();
 		
 		int numOfItems = rand.nextInt(4) + 1; //loot bag can spawn anywhere from 1 to 4 items
-		
+		int randItemId;
 		for (int i=0; i<numOfItems; i++) {
+			randItemId = rand.nextInt(36);
 			//bagItems.add(random item from item class)
+			bagItems.add(Item.getItem(rand));
 		}
-		
-		
-		//add a staff to bag
-		//add a spell to bag
-		//add a robe to bag
 		
 	}
 }
