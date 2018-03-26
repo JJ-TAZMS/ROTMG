ce03c140a5d2539fce60561d36f4cd8e6db7a8fc
James <jamesmichaelm1043@gmail.com>
2018-03-26 19:28:39 -0400

    Minor changes

--- ROTMG/src/game/LootBag.java
+++ ROTMG/src/game/LootBag.java
@@ -7,7 +7,7 @@ public class LootBag extends BufferedImageLoader{
 	
 	ArrayList<String> bagItems; //array list of randomized item id's
 	String tier;
-	Item_Staves Item = new Item_Staves();
+	Item_Wizard itemWiz = new Item_Wizard();
 	//create new lootbag item
 	//to be used in the enemy class when an enemy dies
 	public LootBag(String enemyTier, double xPos, double yPos) {
@@ -30,13 +30,22 @@ public class LootBag extends BufferedImageLoader{
 		
 		Random rand = new Random();
 		
+		int randItemId = rand.nextInt(14);
+		bagItems.add(itemWiz.getRobe(randItemId));
+		randItemId = rand.nextInt(14)+1;
+		bagItems.add(itemWiz.getSpells(randItemId));
+		randItemId = rand.nextInt(7);
+		bagItems.add(itemWiz.getSpells(randItemId));
+		
+		
 		int numOfItems = rand.nextInt(4) + 1; //loot bag can spawn anywhere from 1 to 4 items
-		int randItemId;
+		/*
 		for (int i=0; i<numOfItems; i++) {
 			randItemId = rand.nextInt(36);
 			//bagItems.add(random item from item class)
 			bagItems.add(Item.getItem(rand));
 		}
+		*/
 		
 	}
 }
