8a83420f0dc9df6ca90b398a6d69920c27364eaf
UncleMaho <36541513+UncleMaho@users.noreply.github.com>
2018-03-27 14:57:00 -0400

    actual item class
    ^

---
+++ ROTMG/src/game/Item.java
@@ -0,0 +1,18 @@
+package game;
+
+public class Item 
+{
+	private int atk; //attack
+	private int def; //defense
+	private int spd; //speed
+	private int dex; //dexterity
+	private int vit; //vitality 
+	private int wis; //wisdom 
+	private int hp; //health
+	private int mp; //mana 
+	private String item_name;
+	
+	
+	
+	
+}
